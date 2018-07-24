e9079d31f5180eae19bb36be30a27d1fd48074f3
TheBoneJarmer <rmlabruyere@gmail.com>
2018-07-24 22:34:13 +0200

    Added a lot more functionality

--- src/android/nl/labruyere/PhoneInfo.java
+++ src/android/nl/labruyere/PhoneInfo.java
@@ -20,24 +20,51 @@ import android.util.Log;
 import java.util.Date;
 
 public class PhoneInfo extends CordovaPlugin {
+  public TelephonyManager telephonyManager;
+
   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);    
+    super.initialize(cordova, webView);
+
+    this.telephonyManager = (TelephonyManager)webView.getContext().getSystemService(Context.TELEPHONY_SERVICE);
   }
 
   public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-    if (action.equals("simserialnumber")) {
-      try {
-        TelephonyManager telemamanger = (TelephonyManager) webView.getContext().getSystemService(Context.TELEPHONY_SERVICE);
-        String simSerialNumber = telemamanger.getSimSerialNumber();
-
-        PluginResult result = new PluginResult(PluginResult.Status.OK, simSerialNumber);
-        callbackContext.sendPluginResult(result);
-      } catch (Exception ex) {
-        PluginResult result = new PluginResult(PluginResult.Status.OK, ex.toString());
-        callbackContext.sendPluginResult(result);
+    try {
+      if (action.equals("getSimSerialNumber")) {
+        this.SendResult(callbackContext, this.telephonyManager.getSimSerialNumber());
+        return true;
       }
-    }
 
-    return true;
+      if (action.equals("getDeviceSoftwareVersion")) {
+        this.SendResult(callbackContext, this.telephonyManager.getDeviceSoftwareVersion());
+        return true;
+      }
+
+      if (action.equals("getImei")) {
+        this.SendResult(callbackContext, this.telephonyManager.getImei());
+        return true;
+      }
+
+      if (action.equals("getLine1Number")) {
+        this.SendResult(callbackContext, this.telephonyManager.getLine1Number());
+        return true;
+      }
+
+      if (action.equals("getMeid")) {
+        this.SendResult(callbackContext, this.telephonyManager.getMeid());
+        return true;
+      }
+
+      this.SendResult(callbackContext, "Unknown action " + action);
+      return true;
+    } catch (Exception ex) {
+      this.SendResult(callbackContext, ex.toString());
+      return false;
+    }
+  }
+
+  public void SendResult(CallbackContext callbackContext, String data) {
+    PluginResult result = new PluginResult(PluginResult.Status.OK, data);
+    callbackContext.sendPluginResult(result);
   }
 }
--- www/plugin.js
+++ www/plugin.js
@@ -2,7 +2,19 @@ const exec = require('cordova/exec');
 
 let plugin = { 
   getSimSerialNumber(callback) {
-    exec(callback, null, 'PhoneInfo', 'simserialnumber', null);
+    exec(callback, null, 'PhoneInfo', 'getSimSerialNumber', null);
+  },
+  getDeviceSoftwareVersion(callback) {
+    exec(callback, null, 'PhoneInfo', 'getDeviceSoftwareVersion', null);
+  },
+  getImei(callback) {
+    exec(callback, null, 'PhoneInfo', 'getImei', null);
+  },
+  getPhoneNumber(callback) {
+    exec(callback, null, 'PhoneInfo', 'getLine1Number', null);
+  },
+  getMeid(callback) {
+    exec(callback, null, 'PhoneInfo', 'getMeid', null);
   }
 };
 
